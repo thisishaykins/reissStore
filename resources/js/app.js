61ab569da6463a0430797d968c560fff8d942ee2
Akinshola Samuel <>
2021-06-28 08:25:40 +0100

    Implementations of Frontend Application

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,152 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './views/App.vue'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Register from './views/Register.vue'
+import SingleProduct from './views/SingleProduct.vue'
+import Checkout from './views/Checkout.vue'
+import Confirmation from './views/Confirmation.vue'
+import UserBoard from './views/UserBoard.vue'
+import Admin from './views/Admin.vue'
+import Vendor from './views/Vendor.vue'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: '/products/:id',
+            name: 'single-products',
+            component: SingleProduct
+        },
+        {
+            path: '/confirmation',
+            name: 'confirmation',
+            component: Confirmation
+        },
+        {
+            path: '/checkout',
+            name: 'checkout',
+            component: Checkout,
+            props: (route) => ({ pid: route.query.pid })
+        },
+        {
+            path: '/dashboard',
+            name: 'userboard',
+            component: UserBoard,
+            meta: {
+                requiresAuth: true,
+                is_user: true
+            }
+        },
+        {
+            path: '/admin/:page',
+            name: 'admin-pages',
+            component: Admin,
+            meta: {
+                requiresAuth: true,
+                is_admin: true
+            }
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: Admin,
+            meta: {
+                requiresAuth: true,
+                is_admin: true
+            }
+        },
+        {
+            path: '/vendor/:page',
+            name: 'vendor-pages',
+            component: Vendor,
+            meta: {
+                requiresAuth: true,
+                is_vendor: true
+            }
+        },
+        {
+            path: '/vendor',
+            name: 'vendor',
+            component: Vendor,
+            meta: {
+                requiresAuth: true,
+                is_vendor: true
+            }
+        },
+    ],
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem('reissStore.jwt') == null) {
+            next({
+                path: '/login',
+                params: { nextUrl: to.fullPath }
+            })
+        } else {
+            let user = JSON.parse(localStorage.getItem('reissStore.user'))
+            if (to.matched.some(record => record.meta.is_admin)) {
+                if (user.is_admin == 1) {
+                    next()
+                }
+                else {
+                    next({ name: 'userboard' })
+                }
+            }
+            else if (to.matched.some(record => record.meta.is_user)) {
+                if (user.is_admin == 0) {
+                    next()
+                }
+                else {
+                    next({ name: 'admin' })
+                }
+
+                // if (user.is_vendor == 0) {
+                //     next()
+                // }
+                // else {
+                //     next({ name: 'vendor' })
+                // }
+            }
+            else if (to.matched.some(record => record.meta.is_vendor)) {
+                if (user.is_vendor == 1) {
+                    next()
+                }
+                else {
+                    next({ name: 'userboard' })
+                }
+            }
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
---
+++ resources/js/components/vendor/Users.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <table class="table table-responsive table-striped">
+            <thead>
+                <tr>
+                    <td></td>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Is Admin</td>
+                    <td>Is Vendor</td>
+                    <td>Joined</td>
+                    <td>Total Orders</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(user,index) in users" @key="index">
+                    <td>{{index+1}}</td>
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.is_admin}}</td>
+                    <td>{{user.is_vendor}}</td>
+                    <td>{{user.created_at}}</td>
+                    <td>{{user.orders.length}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                users : []
+            }
+        },
+        beforeMount() {
+            axios.get('/api/users/').then(response => this.users = response.data)
+        }
+    }
+</script>
